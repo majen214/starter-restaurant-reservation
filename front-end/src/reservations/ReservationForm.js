34a71c6661ca516f596219eb799c83a518756cb3
majen214 <88758154+majen214@users.noreply.github.com>
2023-01-28 01:08:57 -0800

    updated files

--- front-end/src/reservations/ReservationForm.js
+++ front-end/src/reservations/ReservationForm.js
@@ -84,7 +84,7 @@ function ReservationForm({ editReservation }) {
 
   function isNumberKey(evt) {
     const charCode = evt.which ? evt.which : evt.keyCode;
-    if (charCode != 45 && charCode > 31 && (charCode < 48 || charCode > 57))
+    if (charCode !== 45 && charCode > 31 && (charCode < 48 || charCode > 57))
       return false;
 
     return true;
